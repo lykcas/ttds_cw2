bb7601d32678cbc59d79d8f7db70807641ca3e61
lykcas <841029817@qq.com>
2019-10-29 18:29:10 +0000

    part1 done

--- ir_evaluation.py
+++ ir_evaluation.py
@@ -1,48 +1,13 @@
 import string
 import copy
+import math
 from collections import defaultdict
-import re
 
 
-# read from qrels file
-f_q = open("qrels.txt")
-lines_q = f_q.readlines()
-qrels = defaultdict(list)
-for line in lines_q:
-    pos1 = line.find(':')
-    query_number = line[0:int(pos1)]
-    query_number = int(query_number)
-    qrels.setdefault(query_number, [])
-    line = line.split()
-    del line[0]
-    trantab = str.maketrans(dict.fromkeys(string.punctuation, ' '))
-    for item in line:
-        item = item.translate(trantab)
-        item = item.split()
-        qrels[query_number].append(item)
-
-for i in range(1, 7):
-    filename = "S" + str(i) + ".results"
-    # read
-    f_S = open(filename)
-    evalname = "S" + str(i) + ".eval"
-
-
-    # read from results file
-    lines = f_S.readlines()
-    Sresults = defaultdict(list)
-    for line in lines:
-        line = line.split()
-        results_number = int(line[0])
-        content_line = line[2: -1]
-        if Sresults.get(results_number):
-            Sresults[results_number].append(content_line)
-        else:
-            Sresults.setdefault(results_number, []).append(content_line)
-
-    # P@10 function
+# P@10 function Sresults, qrels
+def p__10(Sresults, qrels):
     p_at_10 = defaultdict(list)
-    p_10_results = []
+    d = []
     for j in range(1, 11):
         p_at_10.setdefault(j, [])
         top10_Sresults = Sresults[j][0: 10]
@@ -61,9 +26,11 @@ for i in range(1, 7):
             # except ValueError:
             #     flag = 0
         precision = count / 10
-        p_10_results.append(precision)
+        d.append(precision)
+    return d
 
-    # R@50 function
+# R@50 function
+def r__50(Sresults, qrels):
     r_at_50 = defaultdict(list)
     r_50_results = []
     for j in range(1, 11):
@@ -79,8 +46,10 @@ for i in range(1, 7):
                 count += 1
         recall = count / len(qrels[j])
         r_50_results.append(round(recall, 3))
+    return r_50_results
 
-    # R-precision function
+# R-precision function
+def r__p(Sresults, qrels):
     r_p = defaultdict(list)
     r_p_results = []
     for j in range(1, 11):
@@ -97,8 +66,10 @@ for i in range(1, 7):
                 count += 1
         r_precision = count / rel
         r_p_results.append(round(r_precision, 3))
+    return r_p_results
 
-    # MAP
+# MAP
+def map__(Sresults, qrels):
     map_results = []
     for j in range(1, 11):
         ap_individuals = []
@@ -121,15 +92,15 @@ for i in range(1, 7):
             ap += ind
         ap = ap / len(qrels[j])
         map_results.append(round(ap, 3))
+    return map_results
 
-    # nDCG@10 有变量名和前两个function重合，为了区分理解，在重合的变量名前加上了n10
-    import math
+# nDCG@10
+def nDCG__k(Sresults, qrels, sets):
     nDCG_k_results = []
     nDCG_k_retrieved = defaultdict(list)  # {查询：[docno，rank]}
     ideal_retrieved = defaultdict(list)
     nDCG_sets_retrieved = defaultdict(list)
     ideal_sets_retrieved = defaultdict(list)
-    sets = 10
 
     for j in range(1, 11):
         nDCG_k_retrieved.setdefault(j, [])
@@ -182,7 +153,7 @@ for i in range(1, 7):
             ideal_sets_retrieved[j][k-1].append(str(dcgk))
             k += 1
             len_flag = 0
-            if k > 10:
+            if k > sets:
                 len_flag = 1
                 break
 
@@ -199,27 +170,87 @@ for i in range(1, 7):
             ndcgk = dcgk / float(flag)
             ndcgk = round(ndcgk, 3)
         nDCG_k_results.append(str(ndcgk))
+    return nDCG_k_results
+
+# read from qrels file
+if __name__ == '__main__':
+    f_q = open("qrels.txt")
+    lines_q = f_q.readlines()
+    qrels = defaultdict(list)
+    for line in lines_q:
+        pos1 = line.find(':')
+        query_number = line[0:int(pos1)]
+        query_number = int(query_number)
+        qrels.setdefault(query_number, [])
+        line = line.split()
+        del line[0]
+        trantab = str.maketrans(dict.fromkeys(string.punctuation, ' '))
+        for item in line:
+            item = item.translate(trantab)
+            item = item.split()
+            qrels[query_number].append(item)
+
+    f_all = open('All.eval', 'w')
+    f_all.write('\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\n' % ('P@10', 'R@50', 'r-Precision', 'AP', 'nDCG@10', 'nDCG@20'))
+    for i in range(1, 7):
+        filename = "S" + str(i) + ".results"
+        # read
+        f_S = open(filename)
+        evalname = "S" + str(i) + ".eval"
 
 
-    # write in file
-    f__ = open(evalname, 'w')
-    f__.write('\t%-s\t%-s\t%-s\t%-s\t%-s\n' % ('P@10', 'R@50', 'r-Precision', 'AP', 'nDCG@10'))
-    p10 = 0
-    r50 = 0
-    rp = 0
-    ap_ = 0
-    ndcg10 = 0.0
-    ndcg20 = 0
-    for j in range(1, 11):
-        f__.write('%-d\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\n' % (j, p_10_results[j-1], r_50_results[j-1], r_p_results[j-1], map_results[j-1], float(nDCG_k_results[j-1])))
-        p10 += p_10_results[j-1]
-        r50 += r_50_results[j-1]
-        rp += r_p_results[j-1]
-        ap_ += map_results[j-1]
-        ndcg10 += float(nDCG_k_results[j-1])
-    f__.write('%-s\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\n' % ('mean', p10/10, r50/10, rp/10, ap_/10, ndcg10/10))
-    f__.close()
+        # read from results file
+        lines = f_S.readlines()
+        Sresults = defaultdict(list)
+        for line in lines:
+            line = line.split()
+            results_number = int(line[0])
+            content_line = line[2: -1]
+            if Sresults.get(results_number):
+                Sresults[results_number].append(content_line)
+            else:
+                Sresults.setdefault(results_number, []).append(content_line)
 
+        p_10_results = []
+        p_10_results = p__10(Sresults, qrels)
+
+        r_50_results = []
+        r_50_results = r__50(Sresults, qrels)
+
+        r_p_results = []
+        r_p_results = r__p(Sresults, qrels)
+
+        map_results = []
+        map_results = map__(Sresults, qrels)
+
+        nDCG_k_results_10 = []
+        nDCG_k_results_10 = nDCG__k(Sresults, qrels, 10)
+
+        nDCG_k_results_20 = []
+        nDCG_k_results_20 = nDCG__k(Sresults, qrels, 20)
+
+        # write in file
+        f__ = open(evalname, 'w')
+        f__.write('\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\n' % ('P@10', 'R@50', 'r-Precision', 'AP', 'nDCG@10', 'nDCG@20'))
+        p10 = 0
+        r50 = 0
+        rp = 0
+        ap_ = 0
+        ndcg10 = 0.0
+        ndcg20 = 0
+        for j in range(1, 11):
+            f__.write('%-d\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\n' % (j, p_10_results[j-1], r_50_results[j-1], r_p_results[j-1], map_results[j-1], float(nDCG_k_results_10[j-1]), float(nDCG_k_results_20[j-1])))
+            p10 += p_10_results[j-1]
+            r50 += r_50_results[j-1]
+            rp += r_p_results[j-1]
+            ap_ += map_results[j-1]
+            ndcg10 += float(nDCG_k_results_10[j-1])
+            ndcg20 += float(nDCG_k_results_20[j-1])
+        f__.write('%-s\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\n' % ('mean', p10/10, r50/10, rp/10, ap_/10, ndcg10/10, ndcg20/10))
+        f__.close()
+        filename_alleval = 's' + str(i)
+        f_all.write('%-s\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\t%-.3f\n' % (filename_alleval, p10/10, r50/10, rp/10, ap_/10, ndcg10/10, ndcg20/10))
+    f_all.close()
 
 
 
